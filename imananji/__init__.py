828221a8bfa6c6804ed210d0d8aedd0df9575ba7
hiroya Akita <akky.develop@gmail.com>
2020-04-17 19:00:13 +0900

    refactor: ダブルクォーテーションをシングルに変更

--- imananji/__init__.py
+++ imananji/__init__.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 
-def main():
-    print("{}やで".format(datetime.now().strftime('%H:%M:%S')))
 
+def main():
+    print('{}やで'.format(datetime.now().strftime('%H:%M:%S')))
